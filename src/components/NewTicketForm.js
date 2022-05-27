30f705abbccd294d92457b370300da4a6b5b10b2
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-05-27 12:54:38 -0700

    add props as parameter in function component and add a PropTypes for onNewTicketCreation which we defined in TicketControl - this is how we are relaying data back to parent component (callback which 'bypasses' unidirectional data flow)

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { v4 } from 'uuid';
+import PropTypes from "prop-types"; //import PropTypes
 
-function NewTicketForm(){
 
-  {/* Note that we are taking advantage of event.target. event.target gives us access to the 
-  event that was just fired. In this case, we just had a submit event. We can actually grab the 
-  values that came from that submit event. Specifically, we can grab the values based on their 
-  name property. We just need to call event.target.[input-field-name-goes-here].value. */}
+function NewTicketForm(props){  // add props as a parameter
+
+  // Note that we are taking advantage of event.target. event.target gives us access to the 
+  //event that was just fired. In this case, we just had a submit event. We can actually grab the 
+  //values that came from that submit event. Specifically, we can grab the values based on their 
+  //name property. We just need to call event.target.[input-field-name-goes-here].value.
   function handleNewTicketFormSubmission(event){
     event.preventDefault();
     console.log(event.target.names.value);
@@ -33,5 +35,11 @@ function NewTicketForm(){
     </React.Fragment>
   );
 }
+
+// We also need to add PropTypes for our new prop.
+NewTicketForm.propTypes = {
+  //We add a PropTypes for onNewTicketCreation. Remember that this.handleAddingNewTicketToList is passed down to the child component as onNewTicketCreation.
+  onNewTicketCreation: PropTypes.func
+};
 
 export default NewTicketForm;
