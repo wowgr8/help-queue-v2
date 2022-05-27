2796f873bc2bff3645d750cf42b99bcac6fd4523
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-05-27 11:32:17 -0700

    add handleNewTicketFormSubmission function which is triggered when form is submitted. we have to use the function keyword here. We can't just use ES6 class syntax to define our functions because this function is inside another function, not inside a class. The default behavior of an HTML submit button is to submit data and refresh the page. We don't want it to refresh the page so we prevent the default behavior with event.preventDefault(). event.target gives us access to the event that was just fired. In this case, we just had a submit event. We can actually grab the values that came from that submit event. Specifically, we can grab the values based on their name property.

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,7 +1,15 @@
 import React from "react";
 import { v4 } from 'uuid';
 
-function NewTicketForm(props){
+function NewTicketForm(){
+
+  function handleNewTicketFormSubmission(event){
+    event.preventDefault();
+    console.log(event.target.names.value);
+    console.log(event.target.location.value);
+    console.log(event.target.issue.value);
+  }
+
   return(
     <React.Fragment>
       <form onSubmit = {handleNewTicketFormSubmission}>
