3c3085313090c4a86bd41444cf3a3802da0fa694
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-05-26 17:34:43 -0700

    add conditional rendering - if statement sets default view to newticketform, if it's true that formvisibleonstage is false, else it will render ticketlist view

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -12,9 +12,18 @@ class TicketControl extends React.Component {
   }
 
   render(){
+    {/* we create a variable called currentlyVisibleState and set it to null because we haven't determined which component should be rendered yet. */}
+    let currentlyVisibleState = null;
+    {/*  If this.state.formVisibleOnPage is true, the currentlyVisibleState will be set to our NewTicketForm component. ELSE our currentlyVisibleState will be set to our TicketList component. */}
+    if (this.state.formVisibleOnPage){
+      currentlyVisibleState = <NewTicketForm />
+    } else {
+      currentlyVisibleState = <TicketList />
+    }
     return(
       <React.Fragment>
-
+        {/* We only need to use JSX and curly braces for evaluation(of which component should be rendered) inside our return(). */}
+        {currentlyVisibleState}
       </React.Fragment>
     );
   }
