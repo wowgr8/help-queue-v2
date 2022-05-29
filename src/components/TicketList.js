9c8647e50a04411f1bb45c3843b73f442307039c
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-05-29 12:15:45 -0700

    replaced  the index of our iterator as a key. We will use a ticket's actual id property instead. Added a PropType for our new onTicketSelection prop. We will also pass down several props to the Ticket component.

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -6,10 +6,15 @@ function TicketList(props){  // Add props as parameter.
   return(
     <React.Fragment>
       <hr/>
-      {props.ticketList.map((ticket, index) => // Loop through the list passed down from TicketControl.
-        <Ticket names={ticket.names}
+      {props.ticketList.map((ticket) => // Loop through the list passed down from TicketControl. We will no longer use the index of our iterator as a key. We will use a ticket's actual id property instead.
+        //Note that we also have to pass in an id prop. This is because we can't pass a key to a child component as a prop. However, our Ticket component will still need access to its own id, hence a separate id prop which is also set to ticket.id.
+        //We will also pass props.onTicketSelection down to the Ticket component as a prop. The Ticket component will handle determining whether it has been selected - not the TicketList component. For the sake of clarity, we are naming the prop being passed down to the Ticket component whenTicketClicked. Because onTicketSelection is itself a prop from the TicketControl component, it is one of TicketList's props, which is why we need to prefix onTicketSelection with props.
+        <Ticket
+          whenTicketClicked = { props.onTicketSelection } 
+          names={ticket.names}
           location={ticket.location}
           issue={ticket.issue}
+          id={ticket.id}
           key={index} />
       )}
     </React.Fragment>
@@ -18,7 +23,8 @@ function TicketList(props){  // Add props as parameter.
 
 // Add propTypes for ticketList.
 TicketList.propTypes = {
-  ticketList: PropTypes.array
+  ticketList: PropTypes.array,
+  onTicketSelection: PropTypes.func // Finally, we also need to add a PropType: onTicketSelection: PropTypes.func.
 };
 
 export default TicketList;
