408e27f015a1c1447cf7195c2b8f48186d939f13
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-05-29 19:08:15 -0700

    Add Event Handler in EditTicketForm Along with Function to Capture Form Values, also added proptypes and import

--- src/components/EditTicketForm.js
+++ src/components/EditTicketForm.js
@@ -1,13 +1,23 @@
 import React from "react";
 import ReusableForm from "./ReusableForm";
 
-function EditTicketForm (props) {
+
+
+// Lastly, we'll add a function called handleEditTicketFormSubmission that captures form values and triggers the handleEditingTicketInList method in TicketControl. We also need to update our JSX so the event handler in our form refers to our new handleEditTicketFormSubmission function.
+
+function EditTicketForm(props){
+  const { ticket } = props;
+
+  function handleEditTicketFormSubmission(event) {
+    event.preventDefault();
+    props.onEditTicket({names: event.target.names.value, location: event.target.location.value, issue: event.target.issue.value, id: ticket.id});
+  }
+
   return (
     <React.Fragment>
       <ReusableForm 
+        formSubmissionHandler={handleEditTicketFormSubmission} /* new code */ 
         buttonText="Update Ticket" />
     </React.Fragment>
   );
 }
-
-export default EditTicketForm;
