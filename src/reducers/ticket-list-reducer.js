db20b485ee3758c0ea31f30cf28936716cf8f2ec
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-05 12:18:18 -0700

    add logic to reducer to pass second test

--- src/reducers/ticket-list-reducer.js
+++ src/reducers/ticket-list-reducer.js
@@ -1,8 +1,28 @@
-// added just enough code to fail first test successfully
-// Note that our export statement includes default because this file will only have one function inside it - the reducer for our ticket list. That way, when we import the reducer into our test or anywhere else, we can store all the code directly inside a single variable such as ticketListReducer. (in test file)
-// our function has two parameters. The first is the state that will need to be changed while the second is the action that will be applied to that state.
-// Our first parameter has a default value. This is an ES6 feature. We can pass default arguments as parameters to functions. That way, if an argument isn't passed into the parameter when the function is called, the function can just use the default.
+// Now we're ready to add logic to the reducer to pass our new test. Ultimately, a reducer is just a pure function that contains a conditional. What the reducer does is dependent on the action passed in as an argument.
+// We'll use a switch case to do this. A switch case is really just simplified syntax for writing a conditional statement.
+
+// We use object destructuring to destructure the other properties from the action object into the variables names, location and issue.
+// Next, we state that our switch will be based on the action.type. Because the action parameter takes an object, the reducer needs to look at the action's type property to determine the action it should take.
+// As always, we don't want to mutate objects in pure functions. Instead, we use Object.assign() to clone the state object.
+// Object.assign() must take three arguments for this to work:
+  // - The first argument must be an empty object {}. Otherwise, Object.assign() will directly mutate the state we pass in instead of making a clone of it first. We don't want to do that!
+  // - The second argument is the object that will be cloned. In the reducer action below, it's the ticket list state we pass into our function.
+  // - The third argument is the change that should be made to our new copy. This will always be the new ticket that should be added to our ticket list state.
+// Object.assign() creates a new key-value pair where the key is the ticket's id and the value is an object with all of the ticket's properties.
+// We return the value from Object.assign(). Our reducer hasn't altered anything. Instead, it made a copy of the state that was passed in as argument, altered the copy, and then returned the altered copy so it can be used elsewhere in our code.
 export default (state = {}, action) => {
-  // Now fist test will pass.
-  return state;
+  const { names, location, issue, id } = action;
+  switch (action.type) {
+  case 'ADD_TICKET':
+    return Object.assign({}, state, {
+      [id]: {
+        names: names,
+        location: location,
+        issue: issue,
+        id: id
+      }
+    });
+  default:
+    return state;
+  }
 };
