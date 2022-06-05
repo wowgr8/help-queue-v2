0e989cae25d4ae92d2bb1d8b678b84fa8cafc730
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-05 15:34:51 -0700

    add creation of the Redux Store by importing createStore and reducer and instantiated the store. This was done after installing react-redux via 'npm install redux@4.0.5 react-redux@7.1.3 --legacy-peer-deps'

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
+import reducer from './reducers/ticket-list-reducer';
+const store = createStore(reducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
