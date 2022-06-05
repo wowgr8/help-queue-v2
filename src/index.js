763a41fff696cc198fffc07c09721eeb5aa9d783
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-05 15:45:14 -0700

    add notes

--- src/index.js
+++ src/index.js
@@ -9,11 +9,13 @@ import {Provider} from 'react-redux';
 // the <Provider> component will give all of its child components access to the connect() function, which is needed to connect to the Redux store.
 const store = createStore(reducer);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+// Our <App /> component is now a child of the <Provider> component. We've also passed the Redux store in as a prop to <Provider> as well.
+// We won't need to explicitly pass store as a prop through the other components in our tree - it's already being inherited by <App /> and all of its children by way of the <Provider> component.
+ReactDOM.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
