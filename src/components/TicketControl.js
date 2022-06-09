1f26f51e3176af5783bd9d2206a377c3e4466af5
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-08 17:37:28 -0700

    imported actions and replaced lines of code with single action functions we created

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -5,6 +5,7 @@ import TicketDetail from './TicketDetail';
 import EditTicketForm from './EditTicketForm';
 import { connect } from 'react-redux';
 import PropTypes from "prop-types";
+import * as a from './../actions';
 
 
 // It should be clear where we will need to dispatch Redux actions - the exact same place where we previously used setState() to change our form's visibility. When refactoring an application to use Redux instead of React for state, this can be a very helpful way to see where the refactor needs to happen. We don't necessarily need to create new methods in our components. We just need to rewire the relevant methods to use Redux instead of React for state.
@@ -24,10 +25,7 @@ class TicketControl extends React.Component {
 
   handleDeletingTicket = (id) => {
     const { dispatch } = this.props;          
-    const action = {                             
-      type: 'DELETE_TICKET',                  
-      id: id
-    }
+    const action = a.deleteTicket(id);
     dispatch(action);                          
     this.setState({ selectedTicket: null });
   }
@@ -40,14 +38,7 @@ class TicketControl extends React.Component {
   handleEditingTicketInList = (ticketToEdit) => {     
     
     const { dispatch } = this.props;
-    const { id, names, location, issue } = ticketToEdit;
-    const action = {
-      type: 'ADD_TICKET',
-      id: id,
-      names: names,
-      location: location,
-      issue: issue,
-    }
+    const action = a.addTicket(ticketToEdit);
     dispatch(action);
     this.setState({
         editing: false,
@@ -62,18 +53,9 @@ class TicketControl extends React.Component {
 
   handleAddingNewTicketToList = (newTicket) => {
     const { dispatch } = this.props;                       
-    const { id, names, location, issue } = newTicket;      
-    const action = {                                       
-      type: 'ADD_TICKET',
-      id: id,
-      names: names,
-      location: location,
-      issue: issue,
-    }
-    dispatch(action);                                      
-    const action2 = {
-      type: 'TOGGLE_FORM'
-    }              
+    const action = a.addTicket(newTicket);
+    dispatch(action);    
+    const action2 = a.toggleForm();            
     dispatch(action2);
   }
 
@@ -86,9 +68,7 @@ class TicketControl extends React.Component {
       });
     } else {
       const { dispatch } = this.props;
-      const action = {
-        type: 'TOGGLE_FORM'
-      }
+      const action = a.toggleForm();
       dispatch(action);
     }
   }
