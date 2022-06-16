afa1cf5b56b548402d5d01c1b739cdd4ede4c20a
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-15 20:05:54 -0700

    removed comments and add Update_time as action to reducer

--- src/reducers/ticket-list-reducer.js
+++ src/reducers/ticket-list-reducer.js
@@ -1,7 +1,7 @@
 import * as c from './../actions/ActionTypes';
 
 export default (state = {}, action) => {
-  const { names, location, issue, id } = action;
+  const { names, location, issue, id, formattedWaitTime, timeOpen } = action;
   switch (action.type) {
     case c.ADD_TICKET:
       return Object.assign({}, state, {
@@ -13,13 +13,20 @@ export default (state = {}, action) => {
         }
       });
 
-    // You may wonder why we are storing objects within objects in this way now - instead of continuing to use an array. Either will work fine for our use case. However, an object full of key-value pairs is much more similar to a database than an array. That's because both a database and an object have unique keys (as opposed to an array's index, which can change as values are added and removed).
-    // Start by making a copy of the state - then we use the delete function to remove the key-value pair that corresponds to the action. We aren't being fully "pure" here - delete directly alters the object it's called on. However, at the very least, we are making a copy of the original object and altering that one instead of altering the original itself.
-    // Note: Why not do this in a purer way? Well, JavaScript doesn't offer an approach that's both native and functional. There are functional JavaScript libraries that will handle this - for example, the Underscore's .omit() function does the trick - but we will keep things simple here.
     case c.DELETE_TICKET:
       let newState = { ...state };
       delete newState[id];
       return newState; 
+    // default:
+    //   return state;
+      
+
+    case c.UPDATE_TIME:
+      const newTicket = Object.assign({}, state[id], {formattedWaitTime});
+      const updatedState = Object.assign({}, state, {
+        [id]: newTicket
+      });
+      return updatedState;
     default:
       return state;
   }
