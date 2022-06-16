d0f387775a4158fb859834129237d328441190d9
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-15 20:39:52 -0700

    add new properties (formattedWaitTime and timeOpen) and then included them in the ADD_TICKET action

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,13 +10,15 @@ export const toggleForm = () => ({
 
 //// Note that addTicket() will take the entire object and then deconstruct it in the action creator. That will save us the trouble of deconstructing the ticket every time we call addTicket() in our React application.
 export const addTicket = (ticket) => {
-  const { names, location, issue, id } = ticket;
+  const { names, location, issue, id, formattedWaitTime, timeOpen} = ticket;
   return {
     type: 'ADD_TICKET',
     names: names,
     location: location,
     issue: issue,
-    id: id
+    id: id,
+    formattedWaitTime,
+    timeOpen: timeOpen
   }
 }
 
