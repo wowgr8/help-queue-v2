e31369217917d81909388ee0da282ea9e847ab3d
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-18 17:38:40 -0700

    add fix to typo

--- src/firebase.js
+++ src/firebase.js
@@ -28,12 +28,13 @@ export default firebase;
 
 // // Your web app's Firebase configuration
 // const firebaseConfig = {
-//   apiKey: "AIzaSyDT7x1NiLO4GXujAeT_3qh7LQKTvSuDqXU",
-//   authDomain: "redux-help-queue-v2.firebaseapp.com",
-//   projectId: "redux-help-queue-v2",
-//   storageBucket: "redux-help-queue-v2.appspot.com",
-//   messagingSenderId: "658609667628",
-//   appId: "1:658609667628:web:a532494e1e7808bb87db89"
+  // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  // databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  // messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  // appId: process.env.REACT_APP_FIREBASE_APP_ID 
 // };
 
 // // Initialize Firebase
