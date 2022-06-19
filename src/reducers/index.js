6c8ee54aa497320073812651f2eecfce90c89b3d
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-18 18:10:04 -0700

    add firestoreReducer to rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@ import formVisibleReducer from "./form-visible-reducer";
 import ticketListReducer from "./ticket-list-reducer";
 import { combineReducers } from "redux";   // Whenever we create a reducer that combines other reducers, we need to import this function.
 // combineReducers() takes an object as an argument. That object contains key-value pairs. The key represents the state slice while the value represents the reducer that handles actions related to that state slice. Our formVisibleReducer handles the formVisibleOnPage state slice while our ticketListReducer handles the mainTicketList state slice.
+import { firestoreReducer } from 'redux-firestore';
 
 const rootReducer = combineReducers({
   formVisibleOnPage: formVisibleReducer,
-  mainTicketList: ticketListReducer
+  mainTicketList: ticketListReducer,
+  firestore: firestoreReducer
 });
 
 export default rootReducer;
