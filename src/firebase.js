10b3f282c48f70a2cb4de757642c2b184681469c
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-18 18:30:09 -0700

    updated firebase configuration reference to communicate with  new firebase/store version that lessons do not know of. As of now, webpack compiles project successfully - See new notes

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,38 @@
-import * as firebase from 'firebase';
-import 'firebase/firestore';
+// import * as firebase from 'firebase';
+// import 'firebase/firestore';
 
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+//   appId: process.env.REACT_APP_FIREBASE_APP_ID 
+// }
+
+// firebase.initializeApp(firebaseConfig);
+// firebase.firestore();
+
+// export default firebase;
+
+
+
+
+// NOTE: The above may be outdated. The lessons asked to install a previous version of Firebase to prevent possible bugs while following along with the lessons. I could not install that version so I installed with a current version of Firebase. The 
+  // setting in firebase.com recommend I create the configuration file like so (may need to revert to this note):
+
+  // NEW NOTE: Used firebase.com's recommended config reference with minor adjustment. Changed app to firebase so that it works with production index.js 
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -9,33 +41,9 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID 
-}
+};
 
-firebase.initializeApp(firebaseConfig);
-firebase.firestore();
+// Initialize Firebase (NEW NOTE: This was originally const app = ...... & export default app - I changed app to firebase to work with production index.js properly)
+const firebase = initializeApp(firebaseConfig);
 
 export default firebase;
-
-
-// NOTE: The above may be outdated. The lessons asked to install a previous version of Firebase to prevent possible bugs while following along with the lessons. I could not install that version so I installed with a current version of Firebase. The 
-  // setting in firebase.com recommend I create the configuration file like so (may need to revert to this note):
-
-
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-  // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  // databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  // messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-  // appId: process.env.REACT_APP_FIREBASE_APP_ID 
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
