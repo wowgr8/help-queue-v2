1af3493cef056b123e44d876b3566710121621a8
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-19 14:42:41 -0700

    add new header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import ticketsImage from "./../img/tickets.png";
 function Header(){
   return (
     <React.Fragment>
-      <h1>Help Queue</h1>
+      <h1>Help Queue- firevaseConfig branch</h1>
       <img src={ticketsImage} alt="single ticket" />
     </React.Fragment>
   );
