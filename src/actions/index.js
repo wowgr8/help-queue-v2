52fea146a168a9aa61bd261e32645b06df5c73aa
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-20 21:06:47 -0700

    commented out add ticket code which is now handled by firestore.

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,18 +9,18 @@ export const toggleForm = () => ({
 
 
 //// Note that addTicket() will take the entire object and then deconstruct it in the action creator. That will save us the trouble of deconstructing the ticket every time we call addTicket() in our React application.
-export const addTicket = (ticket) => {
-  const { names, location, issue, id, formattedWaitTime, timeOpen} = ticket;
-  return {
-    type: 'ADD_TICKET',
-    names: names,
-    location: location,
-    issue: issue,
-    id: id,
-    formattedWaitTime,
-    timeOpen: timeOpen
-  }
-}
+// export const addTicket = (ticket) => {
+//   const { names, location, issue, id, formattedWaitTime, timeOpen} = ticket;
+//   return {
+//     type: 'ADD_TICKET',
+//     names: names,
+//     location: location,
+//     issue: issue,
+//     id: id,
+//     formattedWaitTime,
+//     timeOpen: timeOpen
+//   }
+// }
 
 export const updateTime = (id, formattedWaitTime) => ({
   type: 'UPDATE_TIME',
