0bebfc7ad10a998c4d747fa9c0ba892e2120da48
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-23 15:21:15 -0700

    add firestore hook to pass in tivkets from Firestore. No longer being passed in from ticketList - see notes

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -1,31 +1,59 @@
 import React from 'react';
 import Ticket from './Ticket';
 import PropTypes from "prop-types";
+// Here we import hooks functionality from both react-redux and react-redux-firebase.
+import {useSelector} from 'react-redux';
+import {useFirestoreConnect, isLoaded, isEmpty} from 'react-redux-firebase';
 
 function TicketList(props){
+  // The useFirestoreConnect() hook comes from react-redux-firebase.
+  useFirestoreConnect([
+    { collection: "tickets" }
+  ]);
 
-  return (
-    <React.Fragment>
-      <hr/>
-      {/* The Ticket.js component needs to have access to the formattedWaitTime property. In order to pass these props to Ticket.js, though, we first need to pass them to TicketList.js, which is the direct parent of Ticket.js and the child of TicketControl.js */}
-      {Object.values(props.ticketList).map((ticket) =>
-        <Ticket 
-          whenTicketClicked = { props.onTicketSelection }
-          names={ticket.names}
-          location={ticket.location}
-          issue={ticket.issue}
-          //* We just add formattedWaitTime as a prop to pass down to Ticket.js. We don't need to pass timeOpen as a prop because we won't be displaying it in Ticket.js. We'll only be showing the formattedWaitTime.
-          formattedWaitTime={ticket.formattedWaitTime}
-          id={ticket.id}
-          key={ticket.id}/>
-      )}
-    </React.Fragment>
-  );
+  //The useSelector() hook comes from react-redux.
+  const tickets = useSelector(state => state.firestore.ordered.tickets);
+
+  //React-redux-firebase also offers a useful isLoaded() function.
+  if(isLoaded(tickets)){
+    return (
+      <React.Fragment>
+        <hr/>
+        {tickets.map((ticket) => {
+          return <Ticket 
+            whenTicketClicked = {props.onTicketSelection}
+            names={ticket.names}
+            location={ticket.location}
+            issue={ticket.issue}
+            formattedWaitTime={ticket.formattedWaitTime}
+            id={ticket.id}
+            key={ticket.id} />
+        })}
+      </React.Fragment>
+    );
+  // if the tickets aren't loaded yet, our fragment will return a "loading.." message.  
+  } else {
+    return (
+      <React.Fragment>
+        <hr/>
+        {Object.values(props.ticketList).map((ticket) =>
+          <Ticket 
+            whenTicketClicked = { props.onTicketSelection }
+            names={ticket.names}
+            location={ticket.location}
+            issue={ticket.issue}
+            formattedWaitTime={ticket.formattedWaitTime}
+            id={ticket.id}
+            key={ticket.id}/>
+        )}
+      </React.Fragment>
+    );
+  }
 }
 
- // The PropType below has been updated - it's now an object, not an array.
 TicketList.propTypes = {
-  ticketList: PropTypes.object,
+  // We no longer need ticketList props.
+  //ticketList: PropTypes.object,
   onTicketSelection: PropTypes.func
 };
 
