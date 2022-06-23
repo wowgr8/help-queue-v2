83bc5f5c8050a03f6f47fbd0c316f4821de4952b
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-22 20:15:26 -0700

    replaced '' with double quotes

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -10,7 +10,7 @@ function NewTicketForm(props){
   function addTicketToFirestore(event) {
     event.preventDefault();
     props.onNewTicketCreation(); 
-    return firestore.collection('tickets').add( 
+    return firestore.collection("tickets").add( 
       {                                         
         names: event.target.names.value,        
         location: event.target.location.value, 
