58a238d891397f35e28ae9c85c13cb28a67c7970
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-26 12:15:03 -0700

    add import for link from react router dom. Added link tags to route signin

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,19 @@
 import React from "react";
 import ticketsImage from "./../img/tickets.png";
+import { Link } from "react-router-dom";
 
 function Header(){
   return (
     <React.Fragment>
       <h1>Help Queue</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/signin">Sign In</Link>
+        </li>
+      </ul>
       <img src={ticketsImage} alt="single ticket" />
     </React.Fragment>
   );
