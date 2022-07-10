c9dcd141d0d9db0d45d2bacc6405b35ffc70486a
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-09 23:28:20 -0700

    removed comments

--- src/__tests__/reducers/ticket-list-reducer.test.js
+++ src/__tests__/reducers/ticket-list-reducer.test.js
@@ -1,7 +1,6 @@
 import ticketListReducer from '../../reducers/ticket-list-reducer';
 import Moment from 'moment';
 
-
 describe('ticketListReducer', () => {
 
   let action;
@@ -25,34 +24,9 @@ describe('ticketListReducer', () => {
     id: 2 }
   };
 
-
   test('Should return default state if ther is no action type passed into the reducer', () => {
     expect(ticketListReducer({}, {type: null})).toEqual({});
   })
-  
-  // test('Should successfully add a ticket to the ticket list that includes Moment-formatted wait times', () => {
-  //   const { names, location, issue, timeOpen, id } = ticketData;
-  //   action = {
-  //     type: 'ADD_TICKET',
-  //     names: names,
-  //     location: location,
-  //     issue: issue,
-  //     timeOpen: timeOpen,
-  //     id: id,
-  //     formattedWaitTime: new Moment().fromNow(true)
-  //   };
-  //   expect(ticketListReducer({}, action)).toEqual({
-  //     [id] : {
-  //       names: names,
-  //       location: location,
-  //       issue: issue,
-  //       timeOpen: timeOpen,
-  //       id: id,
-  //       formattedWaitTime: 'a few seconds'
-  //     }
-  //   });
-  // });
-
   
   test('Should successfully delete a ticket', () => {
     action = {
