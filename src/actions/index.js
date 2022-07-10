f228e424f9e7f3bda36a11721bbed0e384c63e7c
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-07-09 23:28:36 -0700

    removed comments

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,26 +7,8 @@ export const toggleForm = () => ({
   type: 'TOGGLE_FORM'
 });
 
-
-//// Note that addTicket() will take the entire object and then deconstruct it in the action creator. That will save us the trouble of deconstructing the ticket every time we call addTicket() in our React application.
-// export const addTicket = (ticket) => {
-//   const { names, location, issue, id, formattedWaitTime, timeOpen} = ticket;
-//   return {
-//     type: 'ADD_TICKET',
-//     names: names,
-//     location: location,
-//     issue: issue,
-//     id: id,
-//     formattedWaitTime,
-//     timeOpen: timeOpen
-//   }
-// }
-
 export const updateTime = (id, formattedWaitTime) => ({
   type: 'UPDATE_TIME',
   id: id,
   formattedWaitTime: formattedWaitTime
 });
-
-
-// Actions are simply functions that return objects.
